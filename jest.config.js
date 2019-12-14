edc9183e302a5cb53bfe8f5e48f07d3934e015c2
wzr1337 <bartsch98@gmail.com>
2019-12-14 16:12:05 +0100

    test(general): migrate to setupFilesAfterEnv

--- jest.config.js
+++ jest.config.js
@@ -124,7 +124,7 @@ module.exports = {
   // setupFiles: [],
 
   // The path to a module that runs some code to configure or set up the testing framework before each test
-  setupTestFrameworkScriptFile: "<rootDir>/__mocks__/setup.js",
+  setupFilesAfterEnv: ["<rootDir>/__mocks__/setup.js"],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
