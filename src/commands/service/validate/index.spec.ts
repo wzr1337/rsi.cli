aecb354a013fdc70c54a0fe8a73a7718cbd2dbdb
wzr1337 <bartsch98@gmail.com>
2019-12-15 18:11:10 +0100

    test(service.validate): test all branches

--- src/commands/service/validate/index.spec.ts
+++ src/commands/service/validate/index.spec.ts
@@ -6,9 +6,19 @@ describe("service.init",  () => {
     expect(typeof schema).toBe("object");
   });
 
-  it("validate a correct schema", async () => {
+  it("should validate a correct schema", async () => {
     const valid = await validate("./src/__mocks__/dummyschema.json");
     expect(valid).toBeTruthy();
+  });
+
+  it("should fail on invalid schemas", async () => {
+    try {
+      const valid = await validate("./package.json");
+    } catch (error) {
+      expect(typeof error).toBe("object");
+      expect(error.validationErrors).toBeDefined()
+    }
+
   });
 
 })
